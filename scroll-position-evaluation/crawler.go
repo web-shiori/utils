8122a458d5746dfa8a1c07857e9cf838be5ad791
masatora <atarashi.masatora@gmail.com>
2022-01-20 03:19:21 +0900

    impl scroll-position-evaluation

--- scroll-position-evaluation/crawler.go
+++ scroll-position-evaluation/crawler.go
@@ -1,3 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/PuerkitoBio/goquery"
+)
+
+// TODO: 色んな種類のソーシャルブックマークサービスに対応する.
+
+const (
+	HatenaBookmarkHotEntryURL = "https://b.hatena.ne.jp/hotentry/all"
+)
+
+func crawl() ([]string, error) {
+	var urls []string
+	doc, err := get(HatenaBookmarkHotEntryURL)
+	if err != nil {
+		return nil, err
+	}
+
+	doc.Find(".entrylist-contents-title").Each(func(i int, s *goquery.Selection) {
+		url, exsits := s.Find("a").Attr("href")
+		if exsits {
+			urls = append(urls, url)
+		}
+	})
+	return urls, nil
+}
+
+func get(url string) (doc *goquery.Document, err error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		err = fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+		return
+	}
+
+	doc, err = goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return
+	}
+	return
+}
--- scroll-position-evaluation/main.go
+++ scroll-position-evaluation/main.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	// 現状、はてブの人気エントリーのURLをスクレイピングして標準出力に出力するだけ。
+	// スプシにコピペして使う。
+	// スプシに自動で保存しようと思ってたけどそれはやめた。
+	urls, err := crawl()
+	if err != nil {
+		panic(err)
+	}
+	for _, u := range urls {
+		fmt.Println(u)
+	}
+}
